48635ab06d04711a23694c9ce8b6a6ae8f6585c0
Taiwo Babatunde Y <bt22204@essex.ac.uk>
2023-03-25 12:23:09 +0000

    feat: lab streaming layers with bitalino via open signal

--- data_acq.py
+++ data_acq.py
@@ -6,4 +6,11 @@ import pywt
 
 # initialise the data processing function function
 def data_acquisition():
-    return
+    # MAC address for the local device connected to the Bitalino device
+    pc_mac_address = "xx:xx:xx:xx:xx:xx"  # stored in the 'pc_mac_address' variable
+
+    # LSL stream from the Bitalino device resolved via OpenSignals
+    print(
+        f"Awaiting OpenSignals stream from device {pc_mac_address}..."
+    )  # string output to the user
+    streams = resolve_stream("type", pc_mac_address)
