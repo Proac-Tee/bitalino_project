fc7ac7786b4a28a43b5fc83fc7fd4ae448f62385
Taiwo Babatunde Y <babatundetaiwo038@gmail.com>
2023-03-26 22:24:28 +0100

    feat: initialised the pygame window

---
+++ game.py
@@ -0,0 +1,8 @@
+# import the necessary libraries
+import pygame
+
+# Set up the game window
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 500
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Jumping Game")
